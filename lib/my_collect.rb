04e7a38d1dc3aa350dec5871dd61c4b50db83344
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 20:45:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
-  new_collection =
+  new_collection = []
   index = 0 
   while index < collection.length do 
-    yield(collection[index])
+    new_collection << yield(collection[index])
     index += 1 
   end
-  collection 
+  new_collection
 end 
   
 
